dbd8e6429cac801decc17254d0c1edde3e40a285
Parth Pathak <giet.parthpathak@gmail.com>
2025-07-10 02:37:57 +0530

    Code Updated

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,5 +1,5 @@
-@extends('layouts.app')
-
+{{-- @extends('layouts.app') --}}
+@extends('layouts.user_type.auth')
 @section('content')
 <div class="container-fluid py-4">
   <div class="d-flex justify-content-between mb-3">
